cd743809f799993aec018a35e9ba09f2df7209d6
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-10 23:29:52 -0500

    identify all characters and prompt windows

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,18 @@
 // Assignment code here
-
+var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate").onclick = function(){
+  window.prompt("How long do you want your password to be?");
+  window.confirm("Do you want to include UPPERCASE characters?");
+  window.confirm("Do you want to include LOWERCASE characters?");
+  window.confirm("Do you want to include NUMERIC characters?");
+  window.confirm("Do you want to include SPECIAL characters?")
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +25,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
