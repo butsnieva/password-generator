869fe6cf8c9ac948b0a829a450525de6cdafcaba
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-11 12:58:37 -0500

    check password length and numeric input

--- Develop/script.js
+++ Develop/script.js
@@ -6,23 +6,41 @@ var specialChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate").onclick = function(){
-  window.prompt("How long do you want your password to be?");
-  window.confirm("Do you want to include UPPERCASE characters?");
-  window.confirm("Do you want to include LOWERCASE characters?");
-  window.confirm("Do you want to include NUMERIC characters?");
-  window.confirm("Do you want to include SPECIAL characters?")
+  var passwordLength = window.prompt("How long do you want your password to be?");
+  console.log(passwordLength);
+  while (passwordLength <= 7 || passwordLength >= 128) {
+    passwordLength = alert("Password mast be at least 8 characters and no more than 128 characters long.");
+    return generateBtn();
+  }
+  var isANumber = isNaN(passwordLength) === true;
+  if (isANumber){
+    alert("Please enter a number.");
+    generateBtn();
+  }
+  
+
+  // var uppercaseConfirm = window.confirm("Do you want to include UPPERCASE characters?");
+  //   if (uppercaseConfirm) {
+  //     password = (Math.random()*uppercaseChar.length);
+  //     console.log(uppercaseConfirm);
+  //   }
+  // var lowercaseConfirm = window.confirm("Do you want to include LOWERCASE characters?");
+  // var numericConfirm = window.confirm("Do you want to include NUMERIC characters?");
+  // var specialConfirm = window.confirm("Do you want to include SPECIAL characters?");
 }
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
